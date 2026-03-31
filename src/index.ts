a4d8ebd3b9e0511332f1cfc208590c53f551bde9
Joaquin Gimenez <joaquingim77@gmail.com>
2026-03-31 16:33:48 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import * as dotenv from "dotenv";
 import { z } from "zod";
 import * as http from "http";
 import { readFileSync } from "fs";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
